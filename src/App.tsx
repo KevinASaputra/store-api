867006e128aeb07eaf5e3e97bb4147d452041d82
Kevin <kevinadikasaputragithub@gmail.com>
2023-12-22 22:33:15 +0700

    :wrench: Chore: update app store api configuration

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Card } from "./components/Card/Card"
+import { Header } from "./components/Header"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="max-w-7xl mx-auto px-4">
+        <Card />
+      </main>
+    </>
+  )
+}
+
+export default App
