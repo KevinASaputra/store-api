51681b9c87a1ed46b6a5fe33464d3d1bf166797b
Kevin <kevinadikasaputragithub@gmail.com>
2024-01-13 21:38:53 +0700

    :hammer: Chore: update routing pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import App from './App.tsx'
 import './index.css'
 import { store } from './store'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Home } from './pages/Home.tsx'
+import { NotFoundPage } from './pages/404.tsx'
+import { Login } from './pages/Login.tsx'
+
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '/loginPage',
+    element: <Login />,
+    errorElement: <NotFoundPage />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={route} />
     </Provider>
   </React.StrictMode>,
 )
