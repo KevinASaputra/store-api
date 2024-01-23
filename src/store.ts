e08655aec8d662f4e1ba809377867f782b60b29a
Kevin <kevinadikasaputragithub@gmail.com>
2024-01-23 22:37:26 +0700

    :wrench: Chore: update root store reducer

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit"
-import cartSlice from "./common/features/cart/cartSlice"
+import cartSlice from "./features/cart/cartSlice"
 
 
 export const store = configureStore({
